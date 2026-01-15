d5e465053fc57c9e3c1919fb641bad76029342ae
Abhinav7784 <abhinavcoder7744@gmail.com>
2026-01-15 19:56:46 +0530

    JForg as image saver

---
+++ src/main/java/com/example/springboot_jfrog_ci_cd/SpringbootJfrogCiCdApplication.java
@@ -0,0 +1,29 @@
+package com.example.springboot_jfrog_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootJfrogCiCdApplication {
+
+	@GetMapping("/health")
+	public String health() {
+		return "Spring Boot CI/CD with JFrog Artifactory 🚀";
+	}
+
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/Abhinav7784/springboot-jfrog-ci-cd.git
+//	git push -u origin main
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJfrogCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_jfrog_ci_cd/SpringbootJfrogCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jfrog_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJfrogCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
